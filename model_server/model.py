46e6d6a7e3f3074af67ff0583e16cc4e77cdb95a
Shun0212 <shun0212114@outlook.jp>
2025-05-30 00:32:21 +0900

    やっぱ戻しました

--- model_server/model.py
+++ model_server/model.py
@@ -1,7 +1,8 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
 
-model = SentenceTransformer("Shuu12121/CodeSearch-ModernBERT-Owl-Plus")
+model = SentenceTransformer("Shuu12121/CodeSearch-ModernBERT-Owl-2.0-Plus")
 
 def encode_code(codes: list[str]) -> np.ndarray:
     return model.encode(codes, normalize_embeddings=True)
+ß
